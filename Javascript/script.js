0385e6d989e32e36b79052fd04a003b97bbfc5bb
Camansur <33848603+Camansur@users.noreply.github.com>
2021-11-22 13:39:17 -0700

    Collapsible resources page. Styles changed for the resource list and divs. Javascript portion added for the div list to display its content according to the button activity.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,16 @@
 //Image credits from Unsplash Abby Anaday https://unsplash.com/photos/Nnn2Dc6niVU, Jeremy Bezanger https://unsplash.com/photos/k8HniqcdYS4, and Gustavo Quepón https://unsplash.com/photos/pF_2lrjWiJE
 
+//Code references: w3schools.com
+
 //This variable holds the path to the images to rotate through in the slideshow. 
 var imagesPath = ["images/slideshow1.jpg", "images/slideshow2.jpg", "images/slideshow3.jpg"]; //array to hold paths to images for the slide
 
 //Incremental value to select the correct image in the array by index.
 var i = 0;
+var collapsible = document.getElementsByClassName("collapsible");
+
+//Incremental value for collapsible
+var j;
 
 //This function iterates through the imagesPath array to change the image in the slideshow after a specific amount of time (4000ms = 4 seconds in this case)
 function slideshow() {
@@ -19,15 +25,30 @@ function slideshow() {
 	i++;
 
 	//Check if the index is equal to the length of the array, if true reset to 0 to start the slide show from the beginning of the array.
-	if (i == imagesPath.length) {
-
+	if(i == imagesPath.length) {
 		//Set i to 0.
 		i = 0;
-
 	}
 }
 
 //Ensures that the element exists prior to executing the function slideshow.
-if (document.getElementById("slideShowImage") != null) {
+if(document.getElementById("slideShowImage") !== null) {
 	slideshow();
+}
+
+//Resources page button collapsible
+for(j = 0; j < collapsible.length; j++) {
+
+	collapsible[j].addEventListener("click", function() {
+
+		this.classList.toggle("active");
+
+		var content = this.nextElementSibling;
+
+		if(content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		}
+	});
 }
